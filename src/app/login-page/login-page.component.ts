365a63f724edf172fb25648f452c49c1bf16bc08
Jothish JJ <jothishjj11@gmail.com>
2023-05-07 12:53:10 +0000

    Change login page

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { faGoogle, faApple } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent implements OnInit {
+  // fa icons
+  faGoogle = faGoogle;
+  faApple = faApple;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
