d016d7b8137aeac526a99cd0b7f8efba86e02567
Jothish JJ <jothishjj11@gmail.com>
2023-05-08 04:01:46 +0000

    Setup sign up page

--- src/app/signup-page/signup-page.component.ts
+++ src/app/signup-page/signup-page.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { faGoogle, faApple } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-signup-page',
   templateUrl: './signup-page.component.html',
-  styleUrls: ['./signup-page.component.css']
+  styleUrls: ['./signup-page.component.css'],
 })
 export class SignupPageComponent implements OnInit {
+  // fa icons
+  faGoogle = faGoogle;
+  faApple = faApple;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
