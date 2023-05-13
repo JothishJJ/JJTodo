99780b194d48741ae1d3cb6ea0bdd193bebb2833
Jothish JJ <85335732+JothishJJ@users.noreply.github.com>
2023-05-13 11:03:08 +0000

    Add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,10 @@ import { SignupPageComponent } from './signup-page/signup-page.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TodolistComponent } from './todolist/todolist.component';
 import { TaskComponent } from './todolist/task/task.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -30,6 +34,9 @@ import { TaskComponent } from './todolist/task/task.component';
     FormsModule,
     AppRoutingModule,
     FontAwesomeModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent],
